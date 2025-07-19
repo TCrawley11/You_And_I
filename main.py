51a0ef17faf7384568a25e72e43506fca0a8fe43
TCrawley11 <tcrawley@uwo.ca>
2025-07-19 11:19:31 -0400

    google vertex ai config, gemini connection complete

--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from you-and-i!")
-
-
-if __name__ == "__main__":
-    main()
---
+++ src/gemini.py
@@ -0,0 +1,21 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+from google.genai.types import HttpOptions
+
+load_dotenv()
+api_key = os.getenv("GOOGLE_API_KEY")
+
+client = genai.Client(
+    http_options=HttpOptions(api_version="v1")
+)
+
+model="gemini-2.5-flash"
+
+response = client.models.generate_content(
+    model=model,
+    contents=input("Ask Gemini: "),
+)
+
+print(response.text, end="")
